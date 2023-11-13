c94dcb4e92155c270f53a620d8201a88d26f66d8
Md foisal islam~ <islammdfoisal54@gmail.com>
2023-11-13 09:54:30 -0800

    btn disabled and enabled system added

--- hello.js
+++ hello.js
@@ -16,7 +16,9 @@ document.getElementById('btnCheck').addEventListener('click', () => {
   const emojiContainer = document.getElementById('emojiContainer');
   const index = Math.floor(Math.random() * emojis.length);
   emojiContainer.innerHTML = emojis[index];
+  document.getElementById('btnCheck').setAttribute('disabled', true);
   setTimeout(() => {
     emojiContainer.innerHTML = '';
+    document.getElementById('btnCheck').removeAttribute('disabled');
   }, 2000);
 });
